ce89c94da56c0361e79c2c6f863ac3f4f35e3ef5
Phùng Anh Dũng <dungphung447@gmail.com>
2026-04-28 20:47:07 +0700

    Cố tình viết code sai format

--- Bai07/src/main/java/com/review/App.java
+++ Bai07/src/main/java/com/review/App.java
@@ -1,8 +1,8 @@
 package com.review;
 
 public class App {
-public void testMethod() { // Lỗi Checkstyle: Cố tình không thụt lề
-if(true){ // Lỗi Checkstyle: Cố tình thiếu khoảng trắng sau if và trước {
+public                     void                           testMethod() { // Lỗi Checkstyle: Cố tình không thụt lề
+if(true)                                                          { // Lỗi Checkstyle: Cố tình thiếu khoảng trắng sau if và trước {
 System.out.println("Hello Automated Review!"); // Lỗi Checkstyle: Thụt lề sai
 }
 }
